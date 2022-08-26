bb6c9a1c7f40d495d0bdda179867773c5d81fc0c
Angelo <angeloemme80@alice.it>
2022-08-26 16:56:57 +0200

    Aggiunto unit test per il webservice

--- src/test/java/it/angelomassaro/demospringboot/DemoSpringBootApplicationTests.java
+++ src/test/java/it/angelomassaro/demospringboot/DemoSpringBootApplicationTests.java
@@ -1,16 +1,39 @@
 package it.angelomassaro.demospringboot;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class DemoSpringBootApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	private MockMvc mvc;
+	
+	@Autowired
+	private WebApplicationContext wac;
 
+	@Before
+	public void setup() {
+		mvc = MockMvcBuilders.webAppContextSetup(wac).build();
+	}
+	
+	@Test
+	public void testHello() throws Exception{
+		mvc.perform( MockMvcRequestBuilders.get("/demo/menu").contentType(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk())
+		//Aggiungere altri controlli
+		;
+	}
+	
 }
